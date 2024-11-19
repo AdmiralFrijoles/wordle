c4a434db526e4c3c6557d6d2c30f83f01257086e
Jason Gagner <gagner.jason@gmail.com>
2024-11-18 20:57:45 -0500

    Configure for PWA

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    experimental: {
-        optimizePackageImports: ["@chakra-ui/react"]
-    },
     webpack: (config) => {
         config.resolve.fallback = {
             fs: false,
             path: false
         }
         return config;
+    },
+    async headers() {
+        return [{
+            source: '/(.*)',
+            headers: [
+                {
+                    key: 'X-Content-Type-Options',
+                    value: 'nosniff',
+                },
+                {
+                    key: 'X-Frame-Options',
+                    value: 'DENY',
+                },
+                {
+                    key: 'Referrer-Policy',
+                    value: 'strict-origin-when-cross-origin',
+                },
+            ]
+        }];
     }
 };
 
---
+++ src/app/manifest.ts
@@ -0,0 +1,54 @@
+﻿import {MetadataRoute} from "next";
+import {getAppSetting} from "@/lib/settings-service";
+import {SETTING_APP_DESCRIPTION, SETTING_APP_TITLE} from "@/constants/settings";
+
+export default async function manifest(): Promise<MetadataRoute.Manifest> {
+    const title = await getAppSetting<string>(SETTING_APP_TITLE);
+    const description = await getAppSetting<string>(SETTING_APP_DESCRIPTION);
+
+    return {
+        name: title ?? "Wordle",
+        description: description ?? "",
+        start_url: "/",
+        display: "minimal-ui",
+        theme_color: "#0f172a",
+        background_color: "#0f172a",
+        icons: [
+            {
+                "src": "favicon.ico",
+                "sizes": "64x64 32x32 24x24 16x16",
+                "type": "image/x-icon"
+            },
+            {
+                "src": "apple-touch-icon.png",
+                "sizes": "any",
+                "type": "image/png",
+                "purpose": "any"
+            },
+            {
+                "src": "favicon-192.png",
+                "type": "image/png",
+                "sizes": "192x192",
+                "purpose": "any"
+            },
+            {
+                "src": "favicon-512.png",
+                "type": "image/png",
+                "sizes": "512x512",
+                "purpose": "any"
+            },
+            {
+                "src": "favicon-192.png",
+                "type": "image/png",
+                "sizes": "192x192",
+                "purpose": "maskable"
+            },
+            {
+                "src": "favicon-512.png",
+                "type": "image/png",
+                "sizes": "512x512",
+                "purpose": "maskable"
+            }
+        ],
+    }
+}
