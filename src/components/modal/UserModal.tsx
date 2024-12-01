2b530385f304f51d96e24a262ade36c3456567a8
Jason Gagner <gagner.jason@gmail.com>
2024-12-01 16:19:28 -0500

    Reload page upon sign out

--- src/components/modal/UserModal.tsx
+++ src/components/modal/UserModal.tsx
@@ -5,15 +5,21 @@ import HeaderIcon from "@/components/header/HeaderIcon";
 import {UserCircleIcon} from "@heroicons/react/24/outline";
 import BaseModal from "@/components/modal/ModalBase";
 import {useSession} from "next-auth/react";
+import {Button} from "@nextui-org/react";
 
 type Props = {
-    signOutAction: () => void;
+    signOutAction: () => Promise<void>;
     userImage: string | null | undefined;
 }
 
 export default function UserModal({signOutAction, userImage}: Props) {
     const {data: session, status: sessionStatus} = useSession();
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+    async function signOut() {
+        await signOutAction();
+        window.location.reload();
+    }
 
     return (
         <>
@@ -36,10 +42,13 @@ export default function UserModal({signOutAction, userImage}: Props) {
                     <BaseModal title="User" isOpen={isOpen} onOpenChange={onOpenChange}>
                         <h2 className="flex w-full justify-center">Welcome, {session.user.name}</h2>
                         <hr className="mt-4 mb-4 border-gray-500"/>
-                        <form action={signOutAction}>
-                            <button type="submit" className="flex w-full justify-center items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-base">
+                        <form action={signOut}>
+                            <Button
+                                type="submit"
+                                className="flex w-full justify-center items-center rounded-md p-1 bg-indigo-600 hover:bg-indigo-700 text-white"
+                            >
                                 Sign Out
-                            </button>
+                            </Button>
                         </form>
                     </BaseModal>
                 </>
